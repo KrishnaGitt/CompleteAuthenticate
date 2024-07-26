473c5b72cb7e508a5b50e9024b8a40c4cfe623c6
krishna singhal <krishna.singhal@ivycomptech.com>
2024-07-26 12:01:34 +0530

    adding api class

--- backend/Controller/UserRegistor.js
+++ backend/Controller/UserRegistor.js
@@ -1,9 +1,15 @@
 import User from "../UserModel/User.js";
+import { ApiError } from "../Util/ApiError.js";
 
 export const registorUser=async(req,res)=>{
     const {firstName,lastName,age,email,phone,password,confirmpassword}=req.body;
     try {
-
+        
+        if([firstName,lastName,age,email,phone,password,confirmpassword].some((feild)=>{
+            feild.trim()==""
+        })){
+            throw new ApiError(400,"Mandatory feild are missing")
+        } 
         const createdUser=await User.create({
             firstName, lastName, age, email, phone, password, confirmpassword
         })
