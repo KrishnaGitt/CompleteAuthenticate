96c07d6b7af6046985b0f99a3195ee902e26b2c8
krishna singhal <krishna.singhal@ivycomptech.com>
2024-07-28 16:51:31 +0530

    adding home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import HomePage from './HomePage/HomePage';
 import Register from './Register/Register';
+import HomeProduct from "./HomeProduct/HomeProduct"
 // import Login from'./Login/Login'
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <HomeProduct/>
+      {/* <HomePage/> */}
      {/* <Register/> */}
      {/* <Login/> */}
 
--- frontend/src/HomeProduct/HomeProduct.jsx
+++ frontend/src/HomeProduct/HomeProduct.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
 import './HomeProduct.css'
 const HomeProduct = () => {
+  const arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
   return (
-    <div>HomeProduct</div>
+    <>
+          <div className='heading-text'>Restaurants with online food delivery in Hyderabad</div>
+          <div className='product-filter'>
+            <div className='filter-div'>
+              <div className=''></div>
+              <div className=''>Filter</div>
+            </div>
+            <div className='filter-div'>
+              <div className=''></div>
+              <div className=''>Sort By</div>
+            </div>
+            <div className='filter-div'>
+              <div className=''></div>
+              <div className=''>Fast Delivery</div>
+            </div>
+            <div className='filter-div'>
+              <div className=''></div>
+              <div className=''>New on Swiggy</div>
+            </div>
+            <div className='filter-div'>
+              <div className=''></div>
+              <div className=''>Rating 4.0+</div>
+            </div>
+            <div className='filter-div'>
+              <div className=''></div>
+              <div className=''>Pure Veg</div>
+            </div>
+            <div className='filter-div'>
+              <div className=''></div>
+              <div className=''>Offer</div>
+            </div>
+          </div>
+         <div className='main-product-list'>
+      {arr.map((e)=>(
+        <div className='product-div'>
+        <div className='product-img'></div>
+        <div className='info-sec'>
+          <h4 className='rest-name'>Resturant name</h4>
+          <div className='rating-time'>
+            <div className='rating'>12</div>
+            <div className='deliver-time'>Time to deliver</div>
+          </div>
+          <p className='item-sold'>Item we sold</p>
+          <p className='item-sold'>Address</p>
+        </div>
+    </div>
+      ))}
+     
+    </div>
+    </>
+   
   )
 }
 
